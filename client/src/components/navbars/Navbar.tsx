2688dd4115e7fec1f773095e1f7a6eb4cff8e4b7
Johannes Mwasanyila <joachriss@gmail.com>
2024-12-21 16:48:09 +0300

    configured quill output to render plain text without html tags

--- client/src/components/navbars/Navbar.tsx
+++ client/src/components/navbars/Navbar.tsx
@@ -1,32 +1,40 @@
 import { HiMenuAlt2 } from "react-icons/hi";
-import { IoSearchSharp } from "react-icons/io5";
+import { IoCloseSharp, IoSearchSharp } from "react-icons/io5";
 import { MdKeyboardArrowDown } from "react-icons/md";
 
 import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
-import React from "react";
-// import {
-//     ArchiveBoxXMarkIcon,
-//     ChevronDownIcon,
-//     PencilIcon,
-//     Square2StackIcon,
-//     TrashIcon,
-// } from '@heroicons/react/16/solid'
-
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { TrendyPost } from "../posts/TrendyPost";
+import { Link } from "react-router-dom";
 
 export const Navbar = () => {
-    const [isOpen, setOpen] = React.useState(false)
+    const [isOpen, setOpen] = React.useState(false);
+    const [posts, setPosts] = useState<any>([]);
     const handleMenu = () => {
         setOpen(!isOpen)
     }
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+            try {
+                const response = await axios.get('/posts');
+                setPosts(response.data);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+        fetchPosts();
+    }, []);
+
     return (
         <div className="sticky top-0 z-50 w-full bg-white border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700">
             <nav className="w-full">
                 <div className="justify-between flex flex-row p-6 items-center">
                     <div className="flex items-center">
                         <HiMenuAlt2 onClick={handleMenu} className="cursor-pointer" size={27} />
-                        {/* <RiMenuUnfoldLine /> */}
                     </div>
-                    <div className="text-4xl font-bold">goBlog</div>
+                    <Link to="/" className="text-4xl font-bold">goBlog</Link>
                     <div className="flex flex-row gap-x-5 items-center">
                         <IoSearchSharp size={24} />
                         <div className="p-2 bg-gray-400 rounded">Subscribe</div>
@@ -44,7 +52,6 @@ export const Navbar = () => {
                         <div className="px-2 flex justify-center items-center text-md rounded  border-b-4 border-orange-600">Health</div>
                         <div className="px-2 flex justify-center items-center text-md rounded  border-b-4 border-orange-600">Style</div>
                         <div className="px-2 flex justify-center items-center text-md rounded  border-b-4 border-orange-600">Travel</div>
-                        {/* <div className="text-md flex flex-row items-center font-semibold">More <MdKeyboardArrowDown size={22} /></div> */}
                         <div className="text-md flex flex-row items-center font-semibold">
                             <Menu>
                                 <MenuButton className="inline-flex items-center gap-2 rounded-md bg-gray-800 py-1.5 px-3 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:outline-none data-[hover]:bg-gray-700 data-[open]:bg-gray-700 data-[focus]:outline-1 data-[focus]:outline-white">
@@ -96,16 +103,42 @@ export const Navbar = () => {
             <div onClick={handleMenu} className={`absolute top-0 w-full h-screen ${isOpen ? '' : 'hidden'}`}>
 
             </div>
-            <div className={`absolute bg-black bg-opacity-[95%] hidden md:block z-10 top-0 w-[50%] h-screen duration-200 ease-in-out ${isOpen ? 'translate-x-0' : ' -translate-x-full'} `}>
+            <div className={`absolute bg-black bg-opacity-[95%] hidden md:block z-10 top-0 w-[50%] h-screen duration-300 ease-in-out ${isOpen ? 'translate-x-0' : ' -translate-x-full'} `}>
                 <div className="w-full flex flex-row justify-between p-3 mt-2">
                     <div className="text-xl font-bold text-white">Trending</div>
-                    <div className="text-sm text-gray-100 font-bold cursor-pointer p-1 bg-red-800 rounded-md" onClick={handleMenu}>X</div>
+                    <div className="text-sm text-gray-100 font-bold cursor-pointer p-1 bg-red-800 rounded-md" onClick={handleMenu}><IoCloseSharp size={24} /></div>
+                </div>
+                <div className="grid grid-cols-2 mt-5 gap-2">
+                    {posts && posts.length > 0 ?
+                        (
+                            [...posts].reverse().map((post, index) => {
+                                return <TrendyPost key={index} image={post.image} tittle={post.tittle} category={post.category} author={post.author} date={post.updatedAt} _id={post._id} />
+                            })
+                        ) : (
+                            <div className="flex justify-center items-center text-white">No posts found</div>
+                        )
+                    }
+
                 </div>
             </div>
-            <div className={`absolute bg-black bg-opacity-[95%]  md:hidden z-10 top-0 w-[70%] h-screen duration-200 ease-in-out ${isOpen ? 'translate-x-0' : ' -translate-x-full'} `}>
-                <div className="w-full flex flex-row justify-between p-3 mt-2">
+            <div className={`absolute flex p-3 flex-col bg-black bg-opacity-[95%]  md:hidden z-10 top-0 w-[70%] h-screen duration-200 ease-in-out ${isOpen ? 'translate-x-0' : ' -translate-x-full'} `}>
+                <div className="w-full flex flex-row justify-between mt-2">
                     <div className="text-xl font-bold text-white">Menu</div>
                     <div className="text-sm text-gray-100 font-bold cursor-pointer p-1 bg-red-800 rounded-md" onClick={handleMenu}>X</div>
+                </div>
+                <div className="flex flex-col text-white justify-between py-3 gap-2 my-2 border-t-[1px] border-black">
+                    <div className="px-2 py-2 flex rounded  border-b-4 border-orange-600">World</div>
+                    <div className="px-2 py-2 flex rounded  border-b-4 border-orange-600">Technology</div>
+                    <div className="px-2 py-2 flex rounded  border-b-4 border-orange-600">Gossip</div>
+                    <div className="px-2 py-2 flex rounded  border-b-4 border-orange-600">Politics</div>
+                    <div className="px-2 py-2 flex rounded  border-b-4 border-orange-600">Social</div>
+                    <div className="px-2 py-2 flex rounded  border-b-4 border-orange-600">Sport</div>
+                    <div className="px-2 py-2 flex rounded  border-b-4 border-orange-600">Business</div>
+                    <div className="px-2 py-2 flex rounded  border-b-4 border-orange-600">Health</div>
+                    <div className="px-2 py-2 flex rounded  border-b-4 border-orange-600">Style</div>
+                    <div className="px-2 py-2 flex rounded  border-b-4 border-orange-600">Travel</div>
+                    <div className="px-2 py-2 flex rounded  border-b-4 border-orange-600">About</div>
+                    <div className="px-2 py-2 flex rounded  border-b-4 border-orange-600">Contacts</div>
                 </div>
             </div>
         </div>
--- client/src/components/posts/PostCard.tsx
+++ client/src/components/posts/PostCard.tsx
@@ -1,4 +1,5 @@
 
+import ReactMarkdown from 'react-markdown';
 export const PostCard = (props: any) => {
   return (
     <div className='flex flex-col rounded-xl bg-white p-3 shadow-lg border-[1px]'>
@@ -7,7 +8,7 @@ export const PostCard = (props: any) => {
         </div>
         <div className="flex flex-col mt-3 mb-auto">
             <div className="font-bold text-lg line-clamp-1">{props.tittle}</div>
-            <div className="text-md md:text-md line-clamp-3">{props.description}</div>
+            <div className="text-md md:text-md line-clamp-3"  dangerouslySetInnerHTML={{ __html: props.description }}></div>
         </div>
         <div className="flex flex-row justify-between items-center mt-6">
             <div className="flex flex-col">
