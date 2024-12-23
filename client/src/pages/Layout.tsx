d911fa1449526b8dbc0401bc056e737b9f7804d8
Johannes Mwasanyila <joachriss@gmail.com>
2024-12-23 18:07:28 +0300

    Footer was created

--- client/src/pages/Layout.tsx
+++ client/src/pages/Layout.tsx
@@ -1,12 +1,13 @@
 import { Outlet } from 'react-router-dom'
 import { Navbar } from '../components/navbars/Navbar'
-import { MdKeyboardArrowDown } from 'react-icons/md'
+import { Footer } from '../components/Footer'
 
 export const Layout = () => {
   return (
     <div>
         <Navbar/>
         <Outlet/>
+        <Footer/>
     </div>
   )
 }
