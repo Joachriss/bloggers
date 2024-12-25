3bfc168c921979fec57c12f58b6e9a6c606cc52d
Johannes Mwasanyila <joachriss@gmail.com>
2024-12-26 00:47:45 +0300

    recent post part created and fixed react dom url bug

--- client/src/components/posts/RecentPost.tsx
+++ client/src/components/posts/RecentPost.tsx
@@ -2,12 +2,12 @@ import { Link } from 'react-router-dom'
 
 export const RecentPost = (props:any) => {
   return (
-    <Link to={`postcategory/${props.category}`} className='flex flex-row gap-2 items-center'>
-        <div className="overflow-hidden">
-            <img src={ `http://localhost:5173/uploads/images/${props.image}`} alt="post image" />
+    <Link reloadDocument={true} to={`/post/${props._id}`} className='flex flex-row gap-y-2 gap-x-4 items-center'>
+        <div className="overflow-hidden flex items-center min-w-20 max-w-20 max-h-16 min-h-16 rounded-md">
+            <img src={ `http://localhost:8000/uploads/images/${props.image}`} className='w-full scale-110 rounded-md' alt="post image" />
         </div>
-        <div>
-            <div className="text-lg font-bold">{props.tittle}</div>
+        <div className='flex flex-col'>
+            <div className=" line-clamp-2 font-bold">{props.tittle}</div>
             <small>{props.date}</small>
         </div>
     </Link>
