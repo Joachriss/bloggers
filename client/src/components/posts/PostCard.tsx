c5ebd00c56cb23d3b6393c40f4b36b4e074c3113
Johannes Mwasanyila <joachriss@gmail.com>
2025-01-29 20:03:36 +0300

    Modified the postcard component to include different links

--- client/src/components/posts/PostCard.tsx
+++ client/src/components/posts/PostCard.tsx
@@ -1,30 +1,43 @@
 import { Link } from "react-router-dom"
 import { UserAvatar } from "../UserAvatar"
 import DOMPurify from 'dompurify'
+import { FaEdit, FaTrashAlt } from "react-icons/fa";
+import { useContext } from "react";
+import { UserContext } from "../../../context/UserContext";
 
 
 export const PostCard = (props: any) => {
-    const {image,tittle,description,author,date,category} = props;
+    const userContext = useContext(UserContext);
+    const user = userContext?.user;
+    const {image,tittle,description,author,date,category,creator} = props;
     const baseImageUrl = import.meta.env.VITE_BACKEND_BASE_URL || 'http://localhost:8000';
     return (
-        <Link to={`/post/${props._id}`} className='flex flex-col rounded-xl bg-transparent p-3 shadow-lg cursor-pointer'>
-            <div className="mx-auto max-h-52 w-full overflow-hidden rounded-lg">
+        <div className='flex flex-col relative rounded-xl bg-transparent p-3 shadow-lg cursor-pointer'>
+            <Link to={`/post/${props._id}`} className="mx-auto max-h-52 w-full overflow-hidden rounded-lg">
                 <img src={`${baseImageUrl}/${import.meta.env.VITE_BACKEND_POST_IMAGE_URL}/${image}`} loading="lazy" className='rounded-lg object-cover' alt="post picture" />
-            </div>
-            <div className="flex flex-col mt-3 mb-auto">
+            </Link>
+            {
+                user?.id === creator && user?.id && (
+                    <div className="top-5 absolute right-5 p-2 flex flex-row gap-x-2 rounded-full">
+                        <Link to={`/posts/edit/${props._id}`} className="hover:text-white bg-green-600 p-2 rounded-full flex justify-center items-center"><FaEdit size={20}/></Link>
+                        <Link to={`/posts/delete/${props._id}`} className="hover:text-white bg-red-600 p-2 rounded-full flex justify-center items-center"><FaTrashAlt size={20}/></Link>
+                    </div>
+                )
+            }
+            <Link to={`/post/${props._id}`} className="flex flex-col mt-3 mb-auto">
                 <div className="font-bold text-lg line-clamp-1">{tittle}</div>
                 <div className="md:text-md font-medium line-clamp-2" dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(description) }}></div>
-            </div>
+            </Link>
             <div className="flex flex-row justify-between items-center mt-2">
-                <div className="flex flex-row justify-between items-center gap-x-2">
+                <Link to={`/user/profile/${creator}`} className="flex flex-row justify-between items-center gap-x-2">
                     <UserAvatar userImage={image}/>
                     <div className="flex flex-col">
                         <small className="text-gray-950 dark:text-gray-300 font-bold">{author}</small>
                         <small className="text-gray-500 dark:text-gray-400">{date.slice(0,10)}</small>
                     </div>
-                </div>
-                <small className="border-2 bg-gray-300 dark:bg-gray-800 px-2 py-1 flex justify-center items-center rounded-lg">{category}</small>
+                </Link>
+                <Link to={`/posts/category/${category}`} className="border-2 text-xs bg-gray-300 dark:bg-gray-800 px-2 py-1 flex justify-center items-center rounded-lg">{category}</Link>
             </div>
-        </Link>
+        </div>
     )
 }
