065bf330a8bc736529e49638e76b11f8c69f1495
Johannes Mwasanyila <joachriss@gmail.com>
2025-02-19 17:01:57 +0300

    Modified the payment id generator algorithm

--- server/utils/generatePaymentId.js
+++ server/utils/generatePaymentId.js
@@ -1,1 +1,2 @@
-export const generatePaymentId = () => Date.now().toString(36) + Math.random().toString(36).substring(2, 15) ;
+import crypto from 'crypto';
+export const generatePaymentId = () => crypto.randomUUID();
